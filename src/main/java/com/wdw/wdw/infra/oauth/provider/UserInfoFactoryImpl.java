94b6dcd25b294a212dbd46b34c3aae5278991474
road3144 <fnfnfn258@gmail.com>
2022-09-18 17:26:28 +0900

    feat: kakaoLogin

--- src/main/java/com/wdw/wdw/infra/oauth/provider/UserInfoFactoryImpl.java
+++ src/main/java/com/wdw/wdw/infra/oauth/provider/UserInfoFactoryImpl.java
@@ -1,18 +1,24 @@
 package com.wdw.wdw.infra.oauth.provider;
 
 import com.wdw.wdw.exception.InvalidProviderTypeException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
 @Service
+@Slf4j
 public class UserInfoFactoryImpl implements UserInfoFactory{
     @Override
     public OAuth2UserInfo makeUserInfo(OAuth2UserRequest userRequest, Map<String, Object> attributes) throws InvalidProviderTypeException {
         switch (userRequest.getClientRegistration().getRegistrationId()) {
             case "google":
+                log.info("구글 로그인");
                 return new GoogleUserInfo(attributes);
+            case "kakao":
+                log.info("카카오 로그인");
+                return new KakaoUserInfo(attributes);
             default:
                 throw new InvalidProviderTypeException();
         }
